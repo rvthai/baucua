42ceb59cf7f01c41316525f19bc81b19e755975b
jackiedl <luujackie98@gmail.com>
2020-07-20 16:49:56 -0700

    Setup room and lobby logic

--- server/index.js
+++ server/index.js
@@ -2,13 +2,48 @@ const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
+const {createRoom, joinRoom, getUserInRoom, removeUser, findRoom} = require('./room');
+
 const port = 9000;
 
 io.on('connection', (socket) => {
     console.log(socket.id + ' has just connected');
 
-    socket.on('join', (name, room) => {
-        console.log(name + ' has join room ' + room);
+    socket.on('join', ({name, room, host}, callback) => {
+        if (host){
+            const {user,error} = createRoom({id:socket.id, name, room});
+            if (error) return callback(error);
+            socket.join(user.room);
+
+            io.to(user.room).emit("roomdata", {users:getUserInRoom(user.room)} );
+            
+            callback();
+        }else{
+            const {user,error} = joinRoom({id:socket.id, name, room});
+            if (error) return callback(error);
+            socket.join(user.room);
+
+            io.to(user.room).emit("roomdata", {users:getUserInRoom(user.room)} );
+            
+            callback();
+        }
+    })
+
+    socket.on("startgame", ({room}) => {
+        io.to(room).emit("gamestart");
+    })
+
+
+    socket.on("leaveroom", ({id, room})=>{
+        const user = removeUser({id, room});
+        if (user){
+            io.to(user.room).emit("roomdata",{users: getUserInRoom(user.room)});
+            const r = findRoom(room);
+            if (r.length !== 0){
+                const newHost = r[0].players[0];
+                io.to(newHost.room).emit("newhost");
+            }
+        }
     })
 
     socket.on('disconnect', () => {
--- server/room.js
+++ server/room.js
@@ -1,2 +1,53 @@
 const rooms = [];
 
+const createRoom = ({ id, name, room }) => {
+  const r = {roomId:room, active: false, players:[]};
+  const user = {id,name,room};
+  const index = rooms.findIndex((rm) => rm.roomId === room);
+  if (index !== -1){
+    return {error: 'Room already exist'};
+  }
+  r.players.push(user);
+  rooms.push(r);
+  
+  return {user};
+}
+
+const joinRoom = ({ id, name, room }) => {
+  const index = rooms.findIndex((r) => r.roomId === room);
+  const user = {id,name,room};
+  if (index === -1){
+    return {error: 'Room does not exist'};
+  }
+  
+  rooms[index].players.push(user);
+  return {user};
+}
+
+const getUserInRoom = (room) => {
+  const roomList = rooms.filter((r) => r.roomId === room );
+  if (roomList.length === 0){
+    return {error: 'Room does not exist'};
+  }
+  return roomList[0].players;
+  
+}
+
+const removeUser = ({id, room}) => {
+  const index = rooms.findIndex((rm) => rm.roomId === room);
+  const playerList = rooms[index].players;
+  const playerIndex = playerList.findIndex((user) => user.id === id)
+  if (index !== -1){
+    const user = playerList.splice(playerIndex, 1)[0];
+    if (rooms[index].players.length === 0){
+      rooms.splice(index,1);
+    }
+    return user;
+  }
+}
+
+
+const findRoom = (room) => rooms.filter((r) => r.roomId === room);
+
+
+module.exports = {createRoom, joinRoom, getUserInRoom, removeUser,findRoom};
