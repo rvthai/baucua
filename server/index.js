e143d6af3f07fc64bfdc8ab99178ecd2ef26b956
Ricky Thai <36805556+rvthai@users.noreply.github.com>
2020-07-31 12:56:42 -0700

    Fix some disconnect and hosting problems

--- server/index.js
+++ server/index.js
@@ -1,89 +1,96 @@
-const app = require('express')();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http, {
-    pingInterval: 25000,
-    pingTimeout: 60000,
+const app = require("express")();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http, {
+  pingInterval: 25000,
+  pingTimeout: 60000,
 });
 
 const {
-    createRoom, 
-    joinRoom, 
-    getUserInRoom, 
-    removeUser, 
-    findRoom, 
-    getRoom,
-    addBet, 
-    rollDice} = require('./room');
+  createRoom,
+  joinRoom,
+  getUserInRoom,
+  removeUser,
+  findRoom,
+  getRoom,
+  addBet,
+  rollDice,
+} = require("./room");
 
 const port = 9000;
 
-io.on('connection', (socket) => {
-    console.log(socket.id + ' has just connected');
-    
-    //SOCKET JOIN
-    socket.on('join', ({name, room, host}, callback) => {
-        if (host){
-            socket.roomname = room;
-            const {user,error} = createRoom({id:socket.id, name, room});
-            if (error) return callback(error);
-            socket.join(user.room);
-            io.to(user.room).emit("roomdata", {users:getUserInRoom(user.room)} );
-            
-            callback();
-        }else{
-            socket.roomname = room;
-            const {user,error} = joinRoom({id:socket.id, name, room});
-            if (error) return callback(error);
-            socket.join(user.room);
+io.on("connection", (socket) => {
+  console.log(socket.id + " has just connected");
 
-            io.to(user.room).emit("roomdata", {users:getUserInRoom(user.room)} );
-            
-            callback();
-        }
-        if (findRoom(room)[0].active){
-            const gamestate = getRoom({room})
-            io.to(room).emit("gamestart", ({gamestate}));
-            io.to(room).emit("gamestate", ({gamestate}));
-        }
-    })
+  //SOCKET JOIN
+  socket.on("join", ({ name, room, newRoom }, callback) => {
+    socket.roomname = room;
 
-    //SOCKET GAME LOGIC
-    socket.on("startgame", ({room}) => {
-        const gamestate = getRoom({room})
-        io.to(room).emit("gamestart", ({gamestate}));
-    })
+    // Create room if newRoom is true
+    if (newRoom) {
+      const error = createRoom({ id: socket.id, name, room });
+      if (error) return callback(error);
+      callback();
+    }
 
-    socket.on('roll', ({room}) => {
-        const gamestate = rollDice({room});
-        io.to(room).emit("gamestate", ({gamestate}));
-        io.to(room).emit("roll");
-    })
+    // Add socket to specified room
+    const { user, error } = joinRoom({ id: socket.id, name, room });
+    if (error) return callback(error);
+    socket.join(user.room);
 
-    socket.on("bet", ({room, id, amount, animal}) => {
-        const gamestate = addBet({room, id, amount, animal});
-        io.to(room).emit("gamestate", ({gamestate}));
-    })
+    // Emit data back to client
+    const r = findRoom(room);
+    io.to(user.room).emit("players", { players: getUserInRoom(user.room) });
+    io.to(user.room).emit("roomdata", { room: r[0], id: socket.id });
+    callback();
 
-    socket.on('disconnect', () => {
-        console.log(socket.id + ' had left');
-        //anytime for any reason if a socket disconnects the code below will remove that player
-        //from the gameroom
-        const user = removeUser({id:socket.id, room:socket.roomname});
-        if (user){
-            io.to(user.room).emit("roomdata",{users: getUserInRoom(user.room)});
-            const r = findRoom(user.room);
-            const gamestate = getRoom({room:user.room})
-            if (r.length !== 0){
-                const newHost = r[0].players[0];
-                io.to(newHost.room).emit("gamestate", ({gamestate}));
-                io.to(newHost.room).emit("newhost");
-            }
-        }
-    })
+    if (r[0].active) {
+      const gamestate = getRoom({ room });
+      io.to(room).emit("gamestart", { gamestate });
+      io.to(room).emit("gamestate", { gamestate });
+    }
+  });
+
+  //SOCKET GAME LOGIC
+  socket.on("startgame", ({ room }) => {
+    const gamestate = getRoom({ room });
+    io.to(room).emit("gamestart", { gamestate });
+  });
+
+  socket.on("roll", ({ room }) => {
+    const gamestate = rollDice({ room });
+    io.to(room).emit("gamestate", { gamestate });
+    io.to(room).emit("roll");
+  });
+
+  socket.on("bet", ({ room, id, amount, animal }) => {
+    const gamestate = addBet({ room, id, amount, animal });
+    io.to(room).emit("gamestate", { gamestate });
+  });
+
+  // Socket disconnects
+  socket.on("disconnect", () => {
+    console.log(socket.id + " had left");
+    //anytime for any reason if a socket disconnects the code below will remove that player
+    //from the gameroom
+    const user = removeUser({ id: socket.id, room: socket.roomname });
+    if (user) {
+      io.to(user.room).emit("players", { players: getUserInRoom(user.room) });
+
+      const r = findRoom(user.room);
+      if (r.length !== 0) {
+        const newHost = r[0].players[0].id;
+        r[0].host = newHost;
+        io.to(user.room).emit("newhost", r[0].host);
+      }
+
+      if (r.length !== 0 && r[0].active) {
+        const gamestate = getRoom({ room: user.room });
+        io.to(user.room).emit("gamestate", { gamestate });
+      }
+    }
+  });
 });
-
 
 http.listen(9000, () => {
-    console.log('listening on *:9000');
+  console.log("listening on *:9000");
 });
-
