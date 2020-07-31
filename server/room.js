84c07febfa46848bb2db6af39347b3cdfc5d3baa
Ricky Thai <36805556+rvthai@users.noreply.github.com>
2020-07-31 12:55:05 -0700

    Add player colors and host attributes to rooms

--- server/room.js
+++ server/room.js
@@ -1,129 +1,165 @@
 const rooms = [];
 
 const createRoom = ({ id, name, room }) => {
-  const r = {roomId:room, active: false, players:[], bets:[], dice:[]};
-  const user = {id,name,room, total:0, current:0};
+  console.log("id", id);
+  var colors = [
+    "#c04e48", //red
+    "#4a7eac", //blue
+    "#d3c56e", //yellow
+    "#4e9e58", //green
+    "#ca7f3e", //orange
+    "#7fc7b1", //teal
+    "#ca709d", //pink
+    "#903c9c", //purple
+  ];
+  const r = {
+    roomId: room,
+    host: id,
+    active: false,
+    players: [],
+    bets: [],
+    dice: [],
+    colors: colors,
+  };
+  // const color = r.colors.shift();
+  // const user = { id, name, room, total: 0, current: 0, color };
   const index = rooms.findIndex((rm) => rm.roomId === room);
-  if (index !== -1){
-    return {error: 'Room already exist'};
+  if (index !== -1) {
+    return { error: "Room already exist" };
   }
-  r.players.push(user);
+  // r.players.push(user);
   rooms.push(r);
-  
-  return {user};
-}
+
+  // return { user };
+};
 
 const joinRoom = ({ id, name, room }) => {
   const index = rooms.findIndex((r) => r.roomId === room);
-  const user = {id,name,room, total:0, current:0};
-  if (index === -1){
-    return {error: 'Room does not exist'};
+  const color = rooms[index].colors.shift();
+  const user = { id, name, room, total: 0, current: 0, color };
+  if (index === -1) {
+    return { error: "Room does not exist" };
   }
-  
+
   rooms[index].players.push(user);
-  return {user};
-}
+  return { user };
+};
 
 const getUserInRoom = (room) => {
-  const roomList = rooms.filter((r) => r.roomId === room );
-  if (roomList.length === 0){
-    return {error: 'Room does not exist'};
+  const roomList = rooms.filter((r) => r.roomId === room);
+  if (roomList.length === 0) {
+    return { error: "Room does not exist" };
   }
   return roomList[0].players;
-  
-}
+};
 
-const removeUser = ({id, room}) => {
+const removeUser = ({ id, room }) => {
   const index = rooms.findIndex((rm) => rm.roomId === room);
-  if (index !== -1){
+  if (index !== -1) {
     const playerList = rooms[index].players;
-    const playerIndex = playerList.findIndex((user) => user.id === id)
+    const playerIndex = playerList.findIndex((user) => user.id === id);
     const user = playerList.splice(playerIndex, 1)[0];
-    if (rooms[index].players.length === 0){
-      rooms.splice(index,1);
+    rooms[index].colors.unshift(user.color);
+    if (rooms[index].players.length === 0) {
+      rooms.splice(index, 1);
     }
     return user;
   }
-}
+};
 
 const findRoom = (room) => rooms.filter((r) => r.roomId === room);
 
-const getRoom = ({room}) =>{
+const getRoom = ({ room }) => {
   const index = rooms.findIndex((rm) => rm.roomId === room);
-  if (index !== -1){
+  if (index !== -1) {
     rooms[index].active = true;
     return rooms[index];
   }
-  return {error: 'Room does not exist'};
-}
+  return { error: "Room does not exist" };
+};
 
 //GAME FUNCTIONS
-const addBet = ({room, id, amount, animal}) => {
+const addBet = ({ room, id, amount, animal }) => {
   const gameroom = rooms.find((rm) => rm.roomId === room);
-  if (gameroom){
+  if (gameroom) {
     const player = gameroom.players.find((pl) => pl.id === id);
-    if (gameroom && player){
-        const playerbet = gameroom.bets.find((pb) => pb.id === id && pb.animal === animal);
-        if (playerbet){
-          playerbet.amount += amount;
-          gameroom.players[0].total += amount;
-        }else{
-          const bet = {id:id, animal:animal, amount:amount};
-          gameroom.players[0].total += amount;
-          gameroom.bets.push(bet);
-        }
-        player.total -= amount;
-        player.current += amount;
+    if (gameroom && player) {
+      const playerbet = gameroom.bets.find(
+        (pb) => pb.id === id && pb.animal === animal
+      );
+      if (playerbet) {
+        playerbet.amount += amount;
+        gameroom.players[0].total += amount;
+      } else {
+        const bet = { id: id, animal: animal, amount: amount };
+        gameroom.players[0].total += amount;
+        gameroom.bets.push(bet);
+      }
+      player.total -= amount;
+      player.current += amount;
     }
     return gameroom;
   }
-}
+};
 
-const rollDice = ({room}) => {
+const rollDice = ({ room }) => {
   const gameroom = rooms.find((rm) => rm.roomId === room);
-  
-  const animals = ["deer","bau","chicken","fish","crab","shrimp"];
-  let a = animals.length, k, temp;
-  while(--a > 0){
-      k = Math.floor(Math.random()*(a+1));
-      temp = animals[k];
-      animals[k] = animals[a];
-      animals[a] = temp;
+
+  const animals = ["deer", "bau", "chicken", "fish", "crab", "shrimp"];
+  let a = animals.length,
+    k,
+    temp;
+  while (--a > 0) {
+    k = Math.floor(Math.random() * (a + 1));
+    temp = animals[k];
+    animals[k] = animals[a];
+    animals[a] = temp;
   }
 
   const dice = [];
   let die;
-  for (die = 0; die < 3; die++){
-      const index = Math.floor(Math.random()*6);
-      const multiple = dice.find((ani) => ani.animal === animals[index]);
-      if (multiple){
-          multiple.val += 1;
-      }else{
-          const d = {animal: animals[index], val: 1};
-          dice.push(d);
-      }
+  for (die = 0; die < 3; die++) {
+    const index = Math.floor(Math.random() * 6);
+    const multiple = dice.find((ani) => ani.animal === animals[index]);
+    if (multiple) {
+      multiple.val += 1;
+    } else {
+      const d = { animal: animals[index], val: 1 };
+      dice.push(d);
+    }
   }
   gameroom.dice = dice;
   return calculateProfit(room);
-}
+};
 
-const calculateProfit = (room) =>{
+const calculateProfit = (room) => {
   const gameroom = rooms.find((rm) => rm.roomId === room);
-  for (let die = 0; die < gameroom.dice.length; ++die){
-      const bet = gameroom.bets.filter((b) => b.animal === gameroom.dice[die].animal);
-      if (bet.length > 0){
-          for (let win = 0; win < bet.length; ++win){
-              const player = gameroom.players.find((p) => p.id === bet[0].id);
-              player.total += bet[0].amount*gameroom.dice[die].val+bet[0].amount;
-              gameroom.players[0].total -= bet[0].amount*gameroom.dice[die].val;
-          }
+  for (let die = 0; die < gameroom.dice.length; ++die) {
+    const bet = gameroom.bets.filter(
+      (b) => b.animal === gameroom.dice[die].animal
+    );
+    if (bet.length > 0) {
+      for (let win = 0; win < bet.length; ++win) {
+        const player = gameroom.players.find((p) => p.id === bet[0].id);
+        player.total += bet[0].amount * gameroom.dice[die].val + bet[0].amount;
+        gameroom.players[0].total -= bet[0].amount * gameroom.dice[die].val;
       }
+    }
   }
-  for (let p = 0; p < gameroom.players.length; ++p){
-      gameroom.players[p].current = 0;
+  for (let p = 0; p < gameroom.players.length; ++p) {
+    gameroom.players[p].current = 0;
   }
   gameroom.bets = [];
   return gameroom;
-}
+};
 
-module.exports = {createRoom, joinRoom, getUserInRoom, removeUser,findRoom, getRoom, addBet, rollDice};
+module.exports = {
+  createRoom,
+  joinRoom,
+  getUserInRoom,
+  removeUser,
+  findRoom,
+  getRoom,
+  addBet,
+  rollDice,
+};
